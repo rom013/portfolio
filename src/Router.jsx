407667306855e918c8c88b3944fd54d335d2d22a
rom013 <romullomelo0130@gmail.com>
2024-01-27 17:04:49 -0300

    fix: analytics

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Works from "./pages/Works"
-import ReactGA from "react-ga4"
 
 const Router = () => {
-  ReactGA.initialize("G-4Q28CZKNWK")
-  ReactGA.send(window.location.pathname + window.location.search)
   return (
     <Routes>
       <Route
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/main.css'
 import { BrowserRouter } from 'react-router-dom'
-// import RoutersConfig from './routers'
 import Router from './Router'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Analytics/>
     <BrowserRouter>
       <Router />
     </BrowserRouter>
