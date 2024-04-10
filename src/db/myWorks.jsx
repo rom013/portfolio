7d79a0e115874c4f1de5dbd2ee153b9239319df9
rom013 <romullomelo0130@gmail.com>
2024-04-10 20:09:09 -0300

    refactor: update links and docs

--- src/db/myWorks.jsx
+++ src/db/myWorks.jsx
@@ -1,83 +1,83 @@
-const myWorks = [ 
+const myWorks = [
     {
         name: "SpotDev",
-        image: "https://cdn.discordapp.com/attachments/691421631700271114/1194058604987297923/image.png?ex=66147c3b&is=6602073b&hm=5fd1893790f121d37975c388deda24f789d0ba4402544b4df6e6dca69c205269&",
+        image: "https://i.ibb.co/s9hFsvg/spotde.png",
         time: "2023",
         description: "A plataforma SpotDev é um chat de conversas entre devs online que permite aos profissionais da área de ti se comunicarem, trocarem experiências e compartilharem conhecimentos. A plataforma oferece a possibilidade de criar fóruns de conversas específicas e comunidades sobre temas relacionados à tecnologia, programação, desenvolvimento e inovação. A plataforma tem como objetivo conectar os devs, facilitar o aprendizado e estimular a colaboração entre eles.",
         techs: ["React", "TailwindCSS", "Supabase"],
         auth: "rom013",
         urlSite: "https://spot-dev.vercel.app/",
-        video: "https://cdn.discordapp.com/attachments/691421631700271114/1199474096086466580/Video_sem_titulo_Feito_com_o_Clipchamp.mp4?ex=660c804d&is=65fa0b4d&hm=b0e918b8071229ae0a5ff6f2b0b86109c74afc2f389ea6c2b6e9ccddb4630004&"
+        video: "https://cdn.discordapp.com/attachments/691421631700271114/1199474096086466580/Video_sem_titulo_Feito_com_o_Clipchamp.mp4?ex=66282fcd&is=6615bacd&hm=d1b701e0973459be746fab47c124b4d847b57e60b2a2feedf511cb3686729218&"
     },
     {
         name: "NLW - Ignite lab",
-        image: "https://cdn.discordapp.com/attachments/691421631700271114/1194058605360599130/image.png?ex=66147c3c&is=6602073c&hm=f4fd4a33a64e54cc5dbd059fb803a832355c75f539670a57df183a2ff19a53fe&",
+        image: "https://i.ibb.co/G7SWQLZ/ignite.png",
         time: "2022",
         description: "Plataforma de video aulas",
         techs: ["React", "TailwindCSS", "TypeScript", "graphql"],
         auth: "rom013",
         urlSite: "https://ignite-lab-aulas.vercel.app",
-        video: "" 
+        video: ""
     },
     {
         name: "Pawsy",
-        image: "https://cdn.discordapp.com/attachments/691421631700271114/1135317576122118225/Frame_180_1.png?ex=66130ae3&is=660095e3&hm=9263b9b74250fe019135ef82543b8a9bd8d6d9f10df1064198372dabedfee494&",
+        image: "https://i.ibb.co/BC9MFzC/pawsy.png",
         time: "2023",
         description: "A Pawsy é uma plataforma desenvolvida como parte do projeto de TCC do curso técnico em desenvolvimento de sistemas. Com o objetivo de proporcionar mais praticidade e controle na saúde dos animais de estimação.",
         techs: ["React", "TailwindCSS", "NodeJS", "Express", "MySQL"],
         auth: "Rom013",
         urlSite: "https://pawsy-alpha.vercel.app/",
-        video: "https://cdn.discordapp.com/attachments/691421631700271114/1199494864866377789/screen-capture.webm?ex=660c93a5&is=65fa1ea5&hm=c83f64329c7d6fe19a64c8f2a391371c7105ccfe56ec7ba2640b7d52067f009f&" 
+        video: "https://cdn.discordapp.com/attachments/691421631700271114/1199494864866377789/screen-capture.webm?ex=66284325&is=6615ce25&hm=ce4a659549898f1ce2863b3b3386cde72f126b386f96554949c4ca9bf0c043fd&"
     },
     {
         name: "Praia Grande",
-        image: "https://cdn.discordapp.com/attachments/691421631700271114/1195904629565833247/image.png?ex=6611f8fa&is=65ff83fa&hm=10f8c7af23e5908350ec1858fba76de6f1bc43f6b19f379bd6b1c3345f5c8078&",
+        image: "https://i.ibb.co/d6sLtbC/praia.png",
         time: "2022",
         description: "Este site foi criado para divulgar a cidade de Praia Grande, no litoral de São Paulo, para novos visitantes. Aqui você encontra informações sobre a história da cidade, seus aspectos geopolíticos, seus eventos e seus pontos turísticos.",
         techs: ["HTML", "CSS", "Bootstrap", "Javascript"],
         auth: "Rom013",
         urlSite: "https://rom013.github.io/praia-grande/",
-        video: "https://cdn.discordapp.com/attachments/691421631700271114/1199497001381941319/screen-capture_2.webm?ex=660c95a2&is=65fa20a2&hm=39ea9977e37e7132edab806d99a6e423bee23c221b5fab473b5832f6419e8d37&" 
+        video: "https://cdn.discordapp.com/attachments/691421631700271114/1199497001381941319/screen-capture_2.webm?ex=66284522&is=6615d022&hm=f1417609980d8a8059bea3accf5272b6287b5540f384fff05036a7f8bf69d308&"
     },
     {
         name: "Pokedex",
-        image: "https://cdn.discordapp.com/attachments/691421631700271114/1194058605817765918/image.png?ex=66147c3c&is=6602073c&hm=6d1a3253e706e6137c705fe4cbeabcb20bc9be87decf0643b9de6b79f045c672&",
+        image: "https://i.ibb.co/Srz8dT1/pokedex.png",
         time: "2022",
         description: "Pokedex criada em React em conjunto com a API PokeApi",
         techs: ["React", "TailwindCSS"],
         auth: "Rom013",
         urlSite: "https://pokedex-rom013.vercel.app/",
-        video: "https://cdn.discordapp.com/attachments/691421631700271114/1199657598039883846/screen-capture_1.webm?ex=660d2b33&is=65fab633&hm=5d0453cc9cf509b1c06a6a0ff381ae5303beceeea9c22ad39eeb19072703017f&" 
+        video: "https://cdn.discordapp.com/attachments/691421631700271114/1199657598039883846/screen-capture_1.webm?ex=6628dab3&is=661665b3&hm=2f7ff2e121e4e11d1377546396e1572eec4e9ecaa3d497ab0474b91cd9fb0cd5&"
     },
     {
         name: "Alerta de Ofensiva",
-        image: "https://cdn.discordapp.com/attachments/691421631700271114/1199423408438513754/Duo_just-ezgif.com-webp-to-gif-converter.gif?ex=66158b98&is=66031698&hm=9aebc6705bd30ea7b8954e6c72be75926fe9526b366f31110b34eb0df59455d5&",
+        image: "https://i.ibb.co/RYT5krN/duo.gif",
         time: "2024",
         description: "Uma extensão que lembra os usuários para realizar as tarefas do duolingo é uma ferramenta útil para quem não quer perder o ritmo de estudo.",
         techs: ["React", "TailwindCSS"],
         auth: "Rom013",
         urlSite: "https://github.com/rom013/duolingo_extension",
-        video: "" 
+        video: ""
     },
     {
         name: "Melo's burger",
-        image: "https://cdn.discordapp.com/attachments/691421631700271114/1199502163597086740/image.png?ex=660c9a71&is=65fa2571&hm=f80b6973f602ba053cbcdafdf0660f92a16448d11f72a82432b984cdd6ed7ec2&",
+        image: "https://i.ibb.co/FY5nrGf/melo.png",
         time: "2022",
         description: "Landing page de uma hamburgueria",
         techs: ["HTML", "TailwindCSS", "Javascript"],
         auth: "Rom013",
         urlSite: "https://melo-burgers.vercel.app/",
-        video: "https://cdn.discordapp.com/attachments/691421631700271114/1199505129041305740/screen-capture_3.webm?ex=660c9d34&is=65fa2834&hm=88dde3091ac32c7ffd5c3e033eca37cb61037c0990998608cc5a2de6b9e0a0bb&" 
+        video: "https://cdn.discordapp.com/attachments/691421631700271114/1199505129041305740/screen-capture_3.webm?ex=66284cb4&is=6615d7b4&hm=26bf194ad590d46fe49e60c22b30f3dd005aecd640a94a040b566801a85cb703&"
     },
     {
         name: "Apollo",
-        image: "https://cdn.discordapp.com/attachments/691421631700271114/1199662523474006096/Frame_1_1.png?ex=660d2fca&is=65fabaca&hm=ae2bd11355ce1c3a4f8cfc84af706b92f961079b2371bcb7ed7292d8c6b1b091&",
+        image: "https://i.ibb.co/z79bXY5/apollo.png",
         time: "2023",
         description: "Plataforma de cursos",
         techs: ["PHP", "CSS", "Javascript", "SQL"],
         auth: "Rom013",
         urlSite: "",
-        video: "https://cdn.discordapp.com/attachments/691421631700271114/1199561476571152524/screen-capture.webm?ex=660cd1ae&is=65fa5cae&hm=183b550d0be801a8eb32b6b39d2d52ac78043f05042555ed77a0e7caeb7383fe&" 
+        video: "https://cdn.discordapp.com/attachments/691421631700271114/1199561476571152524/screen-capture.webm?ex=6628812e&is=66160c2e&hm=a19d5c1568c685c0c2c62e7a0373d288f1a0835c567695e13916924ad5efc5c5&"
     },
 ]
 
